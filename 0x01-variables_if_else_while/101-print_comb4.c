4bf5b284b4c9ae3d0b7339c978266366c4ee234e
MuhammadSidibe <ymaliyu905@gmail.com>
2023-07-15 14:20:03 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 48; n <= 55; n++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = n +1; m <= 56; m++)
 		{
-			for (l = 50; l <= 58; l++)
+			for (l = m + 1; l <= 57; l++)
 			{
 				if (l > m && m > n)
 				{
