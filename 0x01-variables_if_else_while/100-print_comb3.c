3f300fb92163dedf2eefdd1d4cd86e46200e154a
MuhammadSidibe <ymaliyu905@gmail.com>
2023-07-15 09:23:02 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 48; n <= 56; n++)
 	{
-		for m = 49; m <=57; m++)
+		for (m = 49; m <=57; m++)
 		{
 			if (m >n)
 			{
@@ -19,11 +19,4 @@ int main(void)
 				if (n != 56 || m != 57)
 				{
 					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+					putchar(' '))
