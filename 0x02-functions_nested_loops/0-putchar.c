ab75b1afeae80ba36487d90c6e8cae9ca1ba2b5d
MuhammadSidibe <ymaliyu905@gmail.com>
2023-07-18 14:18:28 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include <main.h>
+#include "main.h"
+
 /**
  * main - print _putchar
- * is written by Muhammad
+ * 
  * Return: 0
  */
-
 int main(void)
 {
- 	putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
