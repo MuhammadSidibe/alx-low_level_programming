8cee0f1ba02b3ad938f27dddb5c530717a62dd47
MuhammadSidibe <ymaliyu905@gmail.com>
2023-07-24 14:17:32 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,23 +7,14 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	for (; str[i] != '\0')
 	{
-		y++;
-		longi++;
+		if ((i % 2) == 0)
+			_putchar(str[1]);
+		else
+			continue;
 	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-		{
-			_putchar(str[0]);
-		}
-	}
-	_putchar('\n');
+	_putchar('\0');
 }
