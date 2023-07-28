1ffd71414c4415758bf95b094184f2e2eec2f09b
MuhammadSidibe <ymaliyu905@gmail.com>
2023-07-28 14:24:35 -0700

    task8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encoder for ROT13 algorithm
+ * @str: pointer to the input string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0' j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
